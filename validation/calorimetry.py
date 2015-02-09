81d6c639844ddb9c9df9d9ade5e1e5f655a57778
Tingjun Yang <tjyang@fnal.gov>
2015-02-08 22:35:33 -0600

    Add 1d dE/dx plots also the ability to compare distributions in two files.

--- validation/calorimetry.py
+++ validation/calorimetry.py
@@ -1,4 +1,29 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+#######################################################################################
+#
+# Name: calorimetry.py
+# 
+# Purpose: Make and save calorimetry validation histograms to a root file.
+#
+# Authors: Tingjun Yang
+#
+# Usage:
+#
+# calorimetry.py <options>
+#
+# Options:
+#
+# --input <inputfile>       - Input AnalysisTree root file.
+#
+# --output <outputfile>     - Output root file that contain histograms.
+#
+# --tracker <tracker name>  - Optional. Can be separated by commas. 
+#                             If not specified, all trackers will be used.
+#
+# --dataset <dataset name>  - Specify a dataset name, singlemu or BNB etc.
+#                             All histograms will be saved in output:calorimetry/dataset
+#
+########################################################################################
 import sys
 from ROOT import TFile, TCanvas, TH1F, TH2F, TProfile
 from ROOT import gDirectory, gROOT
@@ -8,7 +33,7 @@ def main(argv):
     infile = '/pnfs/uboone/scratch/users/tjyang/output/v03_08_01/ana/prod_muminus_0.1-2.0GeV_isotropic_uboone/anahist.root'
     outfile = 'calorimetry.root'
     tracker = ''
-    histdir = 'histdir'
+    dataset = 'histdir'
     args = argv[1:]
     while len(args) > 0:
         if args[0] == '--input' and len(args) > 1:
@@ -20,8 +45,8 @@ def main(argv):
         elif args[0] == '--tracker' and len(args) > 1:
             tracker = args[1]
             del args[0:2]
-        elif args[0] == '--histdir' and len(args) > 1:
-            histdir = args[1]
+        elif args[0] == '--dataset' and len(args) > 1:
+            dataset = args[1]
             del args[0:2]
         else:
             print 'Unkonw option %s' % args[0]
@@ -42,7 +67,7 @@ def main(argv):
     mychain.SetBranchStatus("*",0);
     mychain.SetBranchStatus("StartPoint*",1);
     mychain.SetBranchStatus("EndPoint*",1);
-    mychain.SetBranchStatus("event",1);
+    #mychain.SetBranchStatus("event",1);
 
     hfile = gROOT.FindObject(outfile)
     if hfile:
@@ -52,24 +77,48 @@ def main(argv):
     dir1 = hfile.mkdir('calorimetry')
     dir1.cd()
     
-    if histdir!='':
-        dir2 = dir1.mkdir(histdir)
+    if dataset!='':
+        dir2 = dir1.mkdir(dataset)
         dir2.cd()
 
-    dedxrr = [{},{},{}]
-    filldedxrr = [{},{},{}]
-    pdedxrr = [{},{},{}]
-    fillpdedxrr = [{},{},{}]
+    dedxrr = {}
+    filldedxrr = {}
+    pdedxrr = {}
+    fillpdedxrr = {}
+    dedx = {}
+    filldedx = {}
+
+    dntracks = {}
+    dntrkhits = {}
+    dtrkdedx = {}
+    dtrkresrg = {}
+
     for t in trackers:
         for ipl in range(3):
-            dedxrr[ipl][t] = TH2F("dedxrr%d_%s"%(ipl,t),"%s, Plane = %d;Residual Range (cm);dE/dx (MeV/cm)"%(t,ipl),1000,0,1000,1000,0,20);
-            filldedxrr[ipl][t] = dedxrr[ipl][t].Fill
-            pdedxrr[ipl][t] = TProfile("pdedxrr%d_%s"%(ipl,t),"%s, Plane = %d;Residual Range (cm);dE/dx (MeV/cm)"%(t,ipl),1000,0,1000)
-            fillpdedxrr[ipl][t] = pdedxrr[ipl][t].Fill
-    #        dedxrr[ipl][t].Print()
-        mychain.SetBranchStatus("*_"+t,1)
-
-    #text_file = open("dedx.txt","w")
+            dedxrr[t+str(ipl)] = TH2F("dedxrr%s_%d_%s"%(dataset,ipl,t),"%s, %s, Plane = %d;Residual Range (cm);dE/dx (MeV/cm)"%(dataset,t,ipl),1000,0,1000,1000,0,20);
+            filldedxrr[t+str(ipl)] = dedxrr[t+str(ipl)].Fill
+            pdedxrr[t+str(ipl)] = TProfile("pdedxrr%s_%d_%s"%(dataset,ipl,t),"%s, %s, Plane = %d;Residual Range (cm);dE/dx (MeV/cm)"%(dataset,t,ipl),1000,0,1000)
+            fillpdedxrr[t+str(ipl)] = pdedxrr[t+str(ipl)].Fill
+            dedx[t+str(ipl)] = TH1F("dedx%s_%d_%s"%(dataset,ipl,t),"%s, %s, Plane = %d;dE/dx (MeV/cm); Nhits"%(dataset,t,ipl),100,0,10);
+            filldedx[t+str(ipl)] = dedx[t+str(ipl)].Fill
+        mychain.SetBranchStatus("ntracks_"+t,1)
+        mychain.SetBranchStatus("trkstartx_"+t,1)
+        mychain.SetBranchStatus("trkstarty_"+t,1)
+        mychain.SetBranchStatus("trkstartz_"+t,1)
+        mychain.SetBranchStatus("trkendx_"+t,1)
+        mychain.SetBranchStatus("trkendy_"+t,1)
+        mychain.SetBranchStatus("trkendz_"+t,1)
+        mychain.SetBranchStatus("ntrkhits_"+t,1)
+        mychain.SetBranchStatus("trkdedx_"+t,1)
+        mychain.SetBranchStatus("trkresrg_"+t,1)
+        dntracks[t] = array("h",[0])
+        mychain.SetBranchAddress("ntracks_"+t,dntracks[t])
+        dntrkhits[t] = array("h",[0]*1000*3)
+        mychain.SetBranchAddress("ntrkhits_"+t,dntrkhits[t])
+        dtrkdedx[t] = array("f",[0.0]*1000*3*2000)
+        mychain.SetBranchAddress("trkdedx_"+t,dtrkdedx[t])
+        dtrkresrg[t] = array("f",[0.0]*1000*3*2000)
+        mychain.SetBranchAddress("trkresrg_"+t,dtrkresrg[t])
 
     entries = mychain.GetEntriesFast()
 
@@ -95,7 +144,10 @@ def main(argv):
         EndPointy = mychain.EndPointy[0]
         EndPointz = mychain.EndPointz[0]
         for t in trackers:
-            ntracks = int(mychain.GetLeaf("ntracks_"+t).GetValue()+0.1)
+            #ntracks = int(mychain.GetLeaf("ntracks_"+t).GetValue()+0.1)
+            ntracks = dntracks[t][0]
+            if ntracks > 1000:
+                ntracks = 1000
             for i in range(ntracks):
                 trkstartx = mychain.GetLeaf("trkstartx_"+t).GetValue(i)
                 trkstarty = mychain.GetLeaf("trkstarty_"+t).GetValue(i)
@@ -103,6 +155,7 @@ def main(argv):
                 trkendx = mychain.GetLeaf("trkendx_"+t).GetValue(i)
                 trkendy = mychain.GetLeaf("trkendy_"+t).GetValue(i)
                 trkendz = mychain.GetLeaf("trkendz_"+t).GetValue(i)
+
 
                 if (PointMatch(trkstartx, trkstarty, trkstartz,
                                StartPointx, StartPointy, StartPointz)
@@ -115,20 +168,26 @@ def main(argv):
                     if (Contained(trkstartx, trkstarty, trkstartz)
                         and Contained(trkendx, trkendy, trkendz)):
                         for j in range(3):
-                            ntrkhits = int(mychain.GetLeaf("ntrkhits_"+t).GetValue(i*3+j)+0.1)
+                            #ntrkhits = int(mychain.GetLeaf("ntrkhits_"+t).GetValue(i*3+j)+0.1)
+                            ntrkhits = dntrkhits[t][i*3+j]
                             if ntrkhits > 2000:
                                 ntrkhits = 2000
                             for k in range(ntrkhits):
-                                trkdedx = mychain.GetLeaf("trkdedx_"+t).GetValue(i*3*2000+j*2000+k)
-                                trkresrg = mychain.GetLeaf("trkresrg_"+t).GetValue(i*3*2000+j*2000+k)
+                                #trkdedx = mychain.GetLeaf("trkdedx_"+t).GetValue(i*3*2000+j*2000+k)
+                                #trkresrg = mychain.GetLeaf("trkresrg_"+t).GetValue(i*3*2000+j*2000+k)
+                                trkdedx = dtrkdedx[t][i*3*2000+j*2000+k]
+                                trkresrg = dtrkresrg[t][i*3*2000+j*2000+k]
                                 if trkdedx>0:
-                                    filldedxrr[j][t](trkresrg,trkdedx)
-                                    fillpdedxrr[j][t](trkresrg,trkdedx)
+                                    filldedxrr[t+str(j)](trkresrg,trkdedx)
+                                    fillpdedxrr[t+str(j)](trkresrg,trkdedx)
+                                    if trkresrg>90 and trkresrg<110:
+                                        filldedx[t+str(j)](trkdedx)
     
     for t in trackers:
         for ipl in range(3):
-            del filldedxrr[ipl][t]
-            del fillpdedxrr[ipl][t]
+            del filldedxrr[t+str(ipl)]
+            del fillpdedxrr[t+str(ipl)]
+            del filldedx[t+str(ipl)]
 
     hfile.Write()
 
--- validation/makeplots.py
+++ validation/makeplots.py
@@ -1,92 +1,210 @@
-#!/usr/bin/env python
-import sys
-from ROOT import TFile, TCanvas, TH1F, TH2F, TProfile
-from ROOT import gDirectory, gROOT
+#! /usr/bin/env python
+#######################################################################################
+#
+# Name: makeplots.py
+# 
+# Purpose: Make plots from root files and save them in gif and pdf formats
+#
+# Authors: Tingjun Yang, Sowjanya Gollapinni
+#
+# Usage:
+#
+# makeplots.py <options>
+#
+# Options:
+#
+# --input <inputfile> - Inputfile(s) that contain histograms, can be seperated by commas
+#
+# --calorimetry       - Make calorimetry validation plots
+#
+########################################################################################
+import sys, os
+sys.argv.append( '-b' )
+from ROOT import TFile, TCanvas, TH1F, TH2F, TProfile, TLegend
+from ROOT import gDirectory, gROOT, gPad
 from validation_utilities import *
 
+# Get the object with a given name from a list of objects in the directory
+def GetObject(name,list):
+    for i in list:
+        if i.GetName()==name:
+            return i.ReadObj()
+    return 0
+
+#rearrange stat boxes, from Trish Vahle
+def SortOutStats(pad, fracPadx, fracPady, startx, starty):
+    pad.Update()
+    pad.Modified()
+    numStats = 0
+    list = pad.GetListOfPrimitives()
+    for i in list:
+        if i.InheritsFrom('TH1'):
+            numStats += 1
+    if numStats != 0:
+        xwidth = fracPadx
+        ywidth = fracPady
+        xgap = 0.01
+        ygap = 0.01
+        x2 = startx
+        y2 = starty
+        x1 = x2 - xwidth
+        y1 = y2 - ywidth
+        cnt = 0
+        for i in list:
+            if i.InheritsFrom('TH1'):
+                col = i.GetLineColor()
+                stat = i.FindObject('stats')
+                stat.SetTextColor(col)
+                stat.SetX1NDC(x1)
+                stat.SetX2NDC(x2)
+                stat.SetY1NDC(y1-cnt*(ywidth+ygap))
+                stat.SetY2NDC(y2-cnt*(ywidth+ygap))
+                cnt+=1
+
+# Print help.
+
+def help():
+
+    filename = sys.argv[0]
+    file = open(filename, 'r')
+
+    doprint=0
+    
+    for line in file.readlines():
+        print line
+        if line[2:13] == 'makeplots.py':
+            doprint = 1
+        elif line[0:6] == '######' and doprint:
+            doprint = 0
+        if doprint:
+            if len(line) > 2:
+                print line[2:],
+            else:
+                print
+
+# Make calorimetry validation plots.
 def plotcalorimetry(infile):
-    # open the file
-#    myfile = TFile(infile)
-#    gDirectory.ls()
-#    gDirectory.cd('calorimetry')
-#    gDirectory.ls()
-#    dirlist = myfile.GetListOfKeys()
-#    for dir in dirlist:
-#        print dir.GetName(),dir.GetClassName()
-#        if dir.GetClassName() == 'TDirectoryFile':
-#            gDirectory.cd(dir.GetName())
-#            gDirectory.ls()
+    infiles = infile.split(",")
+    myfile = {}
+    innames = []
     trackers = []
     datasets = []
     muondEdxR = GetMuondEdxR()
-    canvas = {}
-    myfile = TFile(infile)
-    list1 = myfile.GetListOfKeys()
-    for i in list1:
-        if i.GetClassName() == 'TDirectoryFile':
-            myfile.cd(i.GetName())
-            #print gDirectory.ls()
-            list2 = gDirectory.GetListOfKeys()
-            for j in list2:
-                if j.GetClassName() == 'TDirectoryFile':
-                    dataset = '%s'%j.GetName()
-                    if dataset not in datasets:
-                        datasets.append(dataset)
-                    gDirectory.cd(dataset)
-                    if (dataset not in canvas):
-                        canvas[dataset] = canvas.get(dataset,{})
-                        #gDirectory.ls()
-                    list3 = gDirectory.GetListOfKeys()
-                    if len(trackers) == 0:
-                        for k in list3:
-                            name = k.GetName()
-                            match = re.search(r'_(\w+)$',name)
-                            if match:
-                                if match.group(1) not in trackers:
-                                    trackers.append(match.group(1))
-                    for t in trackers:
-                        if t not in canvas[dataset]:
-                            canvas[dataset][t] = TCanvas("can_"+dataset+t,"can_"+dataset+t,1000,800)
-                            canvas[dataset][t].Divide(2,2)
+    candedxrr = {}
+    candedx = {}
+    nplotsdedx = {}
+    legdedx = {}
+    # Open all the input root files.
+    for file in infiles:
+        inname = os.path.splitext(file)[0]
+        if inname not in innames:
+            innames.append(inname)
+        myfile[inname] = TFile(file)
+        list1 = myfile[inname].GetListOfKeys()
+        # Go to directory calorimetry
+        for i in list1:
+            if i.GetClassName() == 'TDirectoryFile':
+                myfile[inname].cd(i.GetName())
+                list2 = gDirectory.GetListOfKeys()
+                # Go to dataset directory
+                for j in list2:
+                    if j.GetClassName() == 'TDirectoryFile':
+                        dataset = '%s'%j.GetName()
+                        if dataset not in datasets:
+                            datasets.append(dataset)
+                        gDirectory.cd(dataset)
+                        list3 = gDirectory.GetListOfKeys()
+                        # Get all tracker names.
+                        if len(trackers) == 0:
+                            for k in list3:
+                                name = k.GetName()
+                                tracker = name[name.rfind("_")+1:]
+                                if tracker not in trackers:
+                                    trackers.append(tracker)
+                        # Loop over all trackers and make dE/dx vs Residual Range and dE/dx plots.
+                        for t in trackers:
+                            #dE/dx vs Residual Range plots.
+                            if inname+dataset+t not in candedxrr:
+                                candedxrr[inname+dataset+t] = TCanvas("candedxrr_"+inname+dataset+t,"candedxrr_"+inname+dataset+t,1000,800)
+                                candedxrr[inname+dataset+t].Divide(2,2)
+                                for i in range(3):
+                                    candedxrr[inname+dataset+t].cd(i+1)
+                                    dedxrr = GetObject('dedxrr%s_%d_%s'%(dataset,i,t), list3)
+                                    if dedxrr:
+                                        dedxrr.Draw("colz")
+                                    pdedxrr = GetObject('pdedxrr%s_%d_%s'%(dataset,i,t), list3)
+                                    if pdedxrr:
+                                        pdedxrr.SetMarkerStyle(20)
+                                        pdedxrr.SetMarkerSize(0.03)
+                                        pdedxrr.Draw("same")
+                                    muondEdxR.SetMarkerStyle(20)
+                                    muondEdxR.SetMarkerSize(0.3)
+                                    muondEdxR.SetMarkerColor(800)
+                                    muondEdxR.SetLineColor(800)
+                                    muondEdxR.SetLineWidth(2)
+                                    muondEdxR.Draw("pc")
+                            # dE/dx plots.
+                            if dataset+t not in candedx:
+                                nplotsdedx[dataset+t] = 0
+                                candedx[dataset+t] = TCanvas("candedx_"+dataset+t,"candedx_"+dataset+t,1000,800)
+                                candedx[dataset+t].Divide(2,2)
+                                legdedx[dataset+t] = TLegend(0.35,0.6,0.6,0.9)
+                            nplotsdedx[dataset+t] += 1
+                            # plot all 3 planes
                             for i in range(3):
-                                canvas[dataset][t].cd(i+1)
-                                for k in list3:
-                                    if k.GetName()=='dedxrr%d_%s'%(i,t):
-                                        k.ReadObj().Draw("colz")
-                                for k in list3:
-                                    if k.GetName()=='pdedxrr%d_%s'%(i,t):
-                                        k.ReadObj().SetMarkerStyle(20)
-                                        k.ReadObj().SetMarkerSize(0.3)
-                                        k.ReadObj().Draw("same")
-                                muondEdxR.SetMarkerStyle(20)
-                                muondEdxR.SetMarkerSize(0.3)
-                                muondEdxR.SetMarkerColor(800)
-                                muondEdxR.SetLineColor(800)
-                                muondEdxR.SetLineWidth(2)
-                                muondEdxR.Draw("pc")
+                                candedx[dataset+t].cd(i+1)
+                                dedx = GetObject('dedx%s_%d_%s'%(dataset,i,t), list3)
+                                if dedx:
+                                    if nplotsdedx[dataset+t] == 1:
+                                        dedx.Draw()
+                                    else:
+                                        dedx.SetLineColor(nplotsdedx[dataset+t])
+                                        dedx.Draw("sames")
+                                        # if new histogram is too high, adjust y range
+                                        if dedx.GetMaximum()*1.1>gPad.GetUymax():
+                                            hist = gPad.GetListOfPrimitives()[0]
+                                            hist.GetYaxis().SetRangeUser(0,dedx.GetMaximum()*1.1)
+                                    if i==0:
+                                        legdedx[dataset+t].AddEntry(dedx,inname,'l')
 
+    #Save dE/dx vs Residula Range plots.
+    for i in innames:
+        for j in datasets:
+            for k in trackers:
+                if i+j+k in candedxrr:
+                    candedxrr[i+j+k].cd()
+                    candedxrr[i+j+k].Update()
+                    candedxrr[i+j+k].Print('dedxrr_%s_%s_%s.gif'%(i,j,k))
+                    candedxrr[i+j+k].Print('dedxrr_%s_%s_%s.pdf'%(i,j,k))
+
+    #Save dE/dx plots.
     for i in datasets:
         for j in trackers:
-            canvas[i][j].Print('dedxrr_%s_%s.gif'%(i,j))
-            canvas[i][j].Print('dedxrr_%s_%s.pdf'%(i,j))
+            if i+j in candedx:
+                for k in range(3):
+                    candedx[i+j].cd(k+1)
+                    legdedx[i+j].Draw()
+                    SortOutStats(gPad,0.3,0.25,0.9,0.9)
+                candedx[i+j].Print('dedx_%s_%s.gif'%(i,j))
+                candedx[i+j].Print('dedx_%s_%s.pdf'%(i,j))
 
                             
-#                            print name
-#                            l = name.rfind("_")
-#                            tracker = name[k+1:]
-#                            print tracker
-
 def main(argv):
 
     infile=''
     calorimetry = 0
     args = argv[1:]
     while len(args) > 0:
-        if args[0] == '--input' and len(args) > 1:
+        if args[0] == '-h' or args[0] == '--help':
+            help()
+            return 0
+        elif args[0] == '--input' and len(args) > 1:
             infile = args[1]
             del args[0:2]
         elif args[0] == '--calorimetry':
             calorimetry = 1
+            del args[0]
+        elif args[0] == '-b':
             del args[0]
         else:
             print 'Unknown option %s' % args[0]
