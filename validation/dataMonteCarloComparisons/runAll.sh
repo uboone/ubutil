515dcab4f2740bb8ea54f734daedbe59004ae4e0
kduffy <kduffy@fnal.gov>
2017-11-21 17:18:19 -0600

    Comment out everything except track plots -- use this for first tests of CI infrastructure to make sure everything is working without overloading it

--- validation/dataMonteCarloComparisons/runAll.sh
+++ validation/dataMonteCarloComparisons/runAll.sh
@@ -10,31 +10,31 @@ source ${UBUTIL_DIR}/bin/setup.sh $1 $2 $3 $4 $5 $6
 g++ -o getTrackInformation ${UBUTIL_DIR}/bin/getTrackInformation.C `root-config --cflags --glibs`
 ./getTrackInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
 
-g++ -o getShowerInformation ${UBUTIL_DIR}/bin/getShowerInformation.C `root-config --cflags --glibs`
-./getShowerInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
+#g++ -o getShowerInformation ${UBUTIL_DIR}/bin/getShowerInformation.C `root-config --cflags --glibs`
+#./getShowerInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
 
-g++ -o getHitInformation ${UBUTIL_DIR}/bin/getHitInformation.C `root-config --cflags --glibs`
-./getHitInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
+#g++ -o getHitInformation ${UBUTIL_DIR}/bin/getHitInformation.C `root-config --cflags --glibs`
+#./getHitInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
 
-g++ -o getFlashInformation ${UBUTIL_DIR}/bin/getFlashInformation.C `root-config --cflags --glibs`
-./getFlashInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "$IS_CI" "$CHISQ_NOTIFIER" 
+#g++ -o getFlashInformation ${UBUTIL_DIR}/bin/getFlashInformation.C `root-config --cflags --glibs`
+#./getFlashInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "$IS_CI" "$CHISQ_NOTIFIER" 
 
-g++ -o getNflsInformation ${UBUTIL_DIR}/bin/getNflsInformation.C `root-config --cflags --glibs`
+#g++ -o getNflsInformation ${UBUTIL_DIR}/bin/getNflsInformation.C `root-config --cflags --glibs`
 #./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "opflashBeam" 10 "$IS_CI" "$CHISQ_NOTIFIER"
 #./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "opflashCosmic" 150 "$IS_CI" "$CHISQ_NOTIFIER"
-./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "simpleFlashBeam" 10 "$IS_CI" "$CHISQ_NOTIFIER" 
-./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "simpleFlashCosmic" 75 "$IS_CI" "$CHISQ_NOTIFIER" 
+#./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "simpleFlashBeam" 10 "$IS_CI" "$CHISQ_NOTIFIER" 
+#./getNflsInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "simpleFlashCosmic" 75 "$IS_CI" "$CHISQ_NOTIFIER" 
 
-g++ -o getCalorimetryInformation ${UBUTIL_DIR}/bin/getCalorimetryInformation.C `root-config --cflags --glibs`
-./getCalorimetryInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "$IS_CI" "$CHISQ_NOTIFIER" 
+#g++ -o getCalorimetryInformation ${UBUTIL_DIR}/bin/getCalorimetryInformation.C `root-config --cflags --glibs`
+#./getCalorimetryInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "$IS_CI" "$CHISQ_NOTIFIER" 
 
 #g++ -o getPMTInformation getPMTInformation.C `root-config --cflags --glibs`
 #./getPMTInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$PE_CUT" "$THRESHOLD" "$CHISQ_NOTIFIER" 
 
 rm getTrackInformation
-rm getShowerInformation
-rm getHitInformation
-rm getFlashInformation
-rm getCalorimetryInformation
-rm getNflsInformation
+#rm getShowerInformation
+#rm getHitInformation
+#rm getFlashInformation
+#rm getCalorimetryInformation
+#rm getNflsInformation
 #rm getPMTInformation
