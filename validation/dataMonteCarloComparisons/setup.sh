9e0fe1d33646310490edc024ffcbea15f2ddb3f0
Adam <a.lister1@lancaster.ac.uk>
2017-12-15 12:42:20 -0600

    nfls taken from variable in case that PE cut <=0

--- validation/dataMonteCarloComparisons/setup.sh
+++ validation/dataMonteCarloComparisons/setup.sh
@@ -27,7 +27,8 @@ export FILE2_DATAORMC=$5
 export FILE2_LABEL=$6
 
 # output dir
-export OUTDIR="."
+export OUTDIR="/uboone/data/users/alister1/dmctest/"
+#export OUTDIR="."
 #export OUTDIR="/pnfs/uboone/scratch/users/uboone/ci_validation/"
 
 # comparison type 0: area normalised 1: not area normalised
