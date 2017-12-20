68ffb16f5009048cb39318dd7e9bc52c7df62642
Adam <a.lister1@lancaster.ac.uk>
2017-12-20 15:07:22 -0600

    ...and the rest follow.

--- validation/dataMonteCarloComparisons/runShowers.sh
+++ validation/dataMonteCarloComparisons/runShowers.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-source setup.sh
+if [[ $UBUTIL_DIR == "/cvmfs"* ]]
+then
+  echo "not running locally."
+  source ${UBUTIL_DIR}/bin/setup.sh $1 $2 $3 $4 $5 $6
+  export IS_LOCAL=0
+else
+  echo "running locally."
+  source setup.sh $1 $2 $3 $4 $5 $6
+  export IS_LOCAL=1
+fi
+
+if [ $IS_LOCAL -eq 1 ]
+then
+
+  mkdir bin
+  cp *.* bin/
+  cd bin
+  export UBUTIL_DIR="${PWD}/../"
+
+fi
 
 # appends trailing backslash to OUTDIR in case where it's not included
 [ "${OUTDIR: -1}" != "/" ] && OUTDIR=${OUTDIR}/
@@ -9,3 +28,8 @@ g++ -o getShowerInformation getShowerInformation.C `root-config --cflags --glibs
 ./getShowerInformation "$FILE1" "$FILE1_DATAORMC" "$FILE1_LABEL" "$FILE2" "$FILE2_DATAORMC" "$FILE2_LABEL" "$OUTDIR" "$COMP_TYPE" "$IS_CI" "$CHISQ_NOTIFIER" 
 
 rm getShowerInformation
+if [ $IS_LOCAL -eq 1 ]
+then
+  cd ..
+  rm -rf bin
+fi
