5b3693bd50f49a871b90215eca91c0b8a632ab67
Adam <a.lister1@lancaster.ac.uk>
2017-12-28 09:49:50 -0600

    removing auto-local running, replace with manual flag for ease

--- validation/dataMonteCarloComparisons/runAll.sh
+++ validation/dataMonteCarloComparisons/runAll.sh
@@ -2,26 +2,18 @@
 
 # remember to use setup.sh to modify your input files and specify your options
 
-if [ -z $UBUTIL_DIR ]
-then
-  echo "running locally."
-  source setup.sh $1 $2 $3 $4 $5 $6
-  export IS_LOCAL=1
-else
-  echo "not running locally."
-  source ${UBUTIL_DIR}/bin/setup.sh $1 $2 $3 $4 $5 $6
-  export IS_LOCAL=0
-fi
+export IS_LOCAL=1
 
 if [ $IS_LOCAL -eq 1 ]
 then
-
-  mkdir bin
-  cp *.* bin/
-  cd bin
-  export UBUTIL_DIR="${PWD}/../"
-
+  # MUST be run from directory containing scripts
+  export UBUTIL_DIR="."
+  mkdir ${UBUTIL_DIR}/bin
+  cp ${UBUTIL_DIR}/*.* ${UBUTIL_DIR}/bin/
+  cd ${UBUTIL_DIR}/bin
 fi
+
+source ${UBUTIL_DIR}/bin/setup.sh $1 $2 $3 $4 $5 $6
 
 # appends trailing backslash to OUTDIR in case where it's not included
 [ "${OUTDIR: -1}" != "/" ] && OUTDIR=${OUTDIR}/
@@ -63,6 +55,6 @@ rm getNflsInformation
 if [ $IS_LOCAL -eq 1 ]
 then
   cd ..
-  rm -rf bin
+  #rm -rf ${UBUTIL_BIN}/bin
   unset UBUTIL_DIR
 fi
