de8368d6812f801262a51b1a65f066cce35ea26f
kduffy <kduffy@fnal.gov>
2018-01-10 17:29:46 -0600

    Adding code to print out chisq from all plots to a text file (called ChisqValues.txt). This will make it possible to track the chisq over time with the CI software

--- validation/dataMonteCarloComparisons/getHitInformation.C
+++ validation/dataMonteCarloComparisons/getHitInformation.C
@@ -292,13 +292,20 @@ void getHitInformation(TString file1name, TString file1_dataormc, TString file1_
     // check chisq if MC/MC comparison
     if (file1_dataormc == "MC" && file2_dataormc == "MC"){
 
+      // Print all chi2 values to a file for tracking over time
+      std::ofstream ChisqFile;
+      ChisqFile.open("ChisqValues.txt", std::ios_base::app);
+      ChisqFile << saveString << " " << chisqv << "\n";
+      ChisqFile.close();
+      
+      // Print names of plots with high chi2 to a separate file
       if (chisqv >= chisqNotifierCut/100.0){
-
+	
         std::ofstream highChisqFile;
         highChisqFile.open("highChisqPlots.txt", std::ios_base::app);
         highChisqFile << saveString << "\n";
         highChisqFile.close();
-
+	
       }
     }
 
