f5480e15193565ffed6eff3f3a2cc80b8cc9cee2
Herbert Greenlee <greenlee@fnal.gov>
2020-07-06 23:07:17 -0500

    Update filter_duplicates.py to better identify parents.

--- scripts/filter_duplicates.py
+++ scripts/filter_duplicates.py
@@ -21,14 +21,29 @@ import extractor_dict
 
 samweb = project_utilities.samweb()
 
+# If file cpid.txt exists, use process id to identify consumed files as parents.
+
+parents = []
+cpid=''
+if os.path.exists('cpid.txt'):
+    lines = open('cpid.txt').readlines()
+    if len(lines) > 0:
+        cpid = lines[0].strip()
+    
+if cpid != '':
+    dim = 'consumer_process_id %s and consumed_status consumed' % cpid
+    parents = samweb.listFiles(dimensions=dim)
+print '%d parents based on sam process id %s' % (len(parents), cpid)
+
 # Extract sam metadata for each artroot file in the local directory.
 
-mddict = {}             # metadata = mddict[filename]
-
-stream_files = {}
 for f in os.listdir('.'):
-    if f.endswith('.root'):
+    if f.endswith('.root') or f.endswith('.pndr'):
         print 'Checking file %s' % f
+        this_file_parents = []
+        for parent in parents:
+            if parent not in this_file_parents:
+                this_file_parents.append(parent)
 
         # Extract sam metadata for this root file as python dictionary.
 
@@ -39,30 +54,44 @@ for f in os.listdir('.'):
         except:
             md = {}
 
+        # Extract internal metadata parents.
+
         if len(md) > 0:
+            if md.has_key('parents'):
+                for pname in parents:
+                    if pname not in this_file_parents:
+                        this_file_parents.append(pname)
+
+        # Loop over parents.
+
+        for pname in this_file_parents:
 
             # Check whether this file is a duplicate.
 
-            if md.has_key('parents'):
-                for parent in md['parents']:
-                    pname = parent['file_name']
-                    if not pname.startswith('CRT'):
-                        print 'Checking parent %s' % pname
-                        dim = 'ischildof: ( file_name %s with availability physical )' % pname
-                        dim += ' and file_type %s' % md['file_type']
-                        dim += ' and file_format %s' % md['file_format']
-                        dim += ' and data_tier %s' % md ['data_tier']
-                        dim += ' and data_stream %s' % md['data_stream']
-                        dim += ' and ub_project.name %s' % md['ub_Project.Name']
-                        dim += ' and ub_project.stage %s%%' % md['ub_Project.Stage']
-                        dim += ' and ub_project.version %s' % md['ub_Project.Version']
-                        s = samweb.listFilesSummary(dimensions=dim)
-                        nf = s['file_count']
-                        if nf != 0:
+            if not pname.startswith('CRT'):
+                print 'Checking parent %s' % pname
+                dim = 'ischildof: ( file_name %s with availability physical )' % pname
+                if md.has_key('file_type'):
+                    dim += ' and file_type %s' % md['file_type']
+                if md.has_key('file_format'):
+                    dim += ' and file_format %s' % md['file_format']
+                if md.has_key('data_tier'):
+                    dim += ' and data_tier %s' % md ['data_tier']
+                if md.has_key('data_stream'):
+                    dim += ' and data_stream %s' % md['data_stream']
+                if md.has_key('ub_Project.Name'):
+                    dim += ' and ub_project.name %s' % md['ub_Project.Name']
+                if md.has_key('ub_Project.Stage'):
+                    dim += ' and ub_project.stage %s%%' % md['ub_Project.Stage']
+                if md.has_key('ub_Project.Version'):
+                    dim += ' and ub_project.version %s' % md['ub_Project.Version']
+                s = samweb.listFilesSummary(dimensions=dim)
+                nf = s['file_count']
+                if nf != 0:
 
-                            # This file is a duplicate.  Remove it.
+                    # This file is a duplicate.  Remove it.
 
-                            print 'Deleting duplicate file %s.' % f
-                            os.remove(f)
-                            break         # Break out of loop over parents.
+                    print 'Deleting duplicate file %s.' % f
+                    os.remove(f)
+                    break         # Break out of loop over parents.
 
